b1375bce7fb0ec0c8622a2e1ca898433d5c9d391
rended <rended83@gmail.com>
2019-11-27 12:04:12 +0100

    wordcloud

--- component/storylet-creator/src/components/container/menu-container.js
+++ component/storylet-creator/src/components/container/menu-container.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
 import WordcloudContainer from './wordcloud-container'
@@ -11,41 +11,40 @@ import { faLightbulb } from '@fortawesome/free-regular-svg-icons'
 
 export default function MenuContainer() {
 
-    // axios.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=ebb2ab66-af3f-42c9-bc24-8072f0c413d5', {
+    // const [searchKey, setSearchKey] = useState('');
+    const [words, setWords] = useState([]);
+    const searchKey = useRef(null);
+
+    // {
     //     text: 'cane',
     //     lang: 'it', // en fr de pt ru es
     //     type: 'response', // stimulus
     //     limit: '50', // max 300
     //     pos: 'noun,adjective,verb,adverb',
     //     indent: 'yes' // no
-    // })
-    // .then((response) => {
-    //     console.log(response);
-    // }, (error) => {
-    //     console.log(error);
-    // });
+    // }
 
-    https://api.wordassociations.net/associations/v1.0/json/search?apikey=ebb2ab66-af3f-42c9-bc24-8072f0c413d5
-
-    axios.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=ebb2ab66-af3f-42c9-bc24-8072f0c413d5&text=welcome&lang=en&limit=6')
-        .then((response) => {
-            console.log(response.data.response[0].items);
-        }, (error) => {
-            console.log(error);
-        });
-
+    const get_words = async () =>
+    {
+        axios.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=ebb2ab66-af3f-42c9-bc24-8072f0c413d5&text=' + searchKey.current.value + '&lang=it&limit=50')
+            .then((response) => {
+                setWords(response.data.response[0].items)
+            }, (error) => {
+                console.log(error);
+            });
+    };
 
     return (
         <>
             <div id="menu-container">
                 <div className="buttons" > </div>
                 <div className="find-ideas">
-                    <FontAwesomeIcon icon={faLightbulb} className="icon" />
-                    <input className="form-control col-md-10 col-sm-10" />
+                    <FontAwesomeIcon icon={faLightbulb} className="icon" onClick={get_words} />
+                    <input id="SearchKey" ref={searchKey} className="form-control col-md-10 col-sm-10" />
                 </div>
             </div>
 
-            <WordcloudContainer />
+            <WordcloudContainer words={words} />
         </>
     )
 };
---
+++ component/storylet-creator/src/components/wordcloud/wordcloud-component.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect, useRef} from 'react';
+import 'd3-transition';
+import { select } from 'd3-selection';
+import ReactWordcloud from 'react-wordcloud'; // https://react-wordcloud.netlify.com/usage/options
+
+export default function WordcloudComponent(response) {
+
+    console.log("ciao, sono wc component!");
+
+    useEffect(()=>{
+        let words = [];
+        for(let i=0; i<response.words.length; i++) {
+            words.push({
+                text: response.words[i].item,
+                value: response.words[i].weight,
+                color: colors[response.words[i].pos]
+            });
+        }
+        setWords(words);
+        console.log("wc-->response");
+    }, [response]);
+
+    const [words, setWords] = useState([]);
+
+    const colors = {noun:'#4CAF50',adjective:'#F44336',verb:'#FF9800',adverb:'#2196F3'};
+
+    const options = {
+        fontSizes: [20, 100],
+        rotations: 3,
+        rotationAngles: [0, 90],
+        padding: 1
+    };
+
+    const callbacks = {
+        getWordColor: word => (word.color),
+        getWordTooltip: word =>
+            `La parola "${word.text}" ha un peso pari a ${word.value}.`,
+        onWordClick: getCallback('onWordClick'),
+        onWordMouseOut: getCallback('onWordMouseOut'),
+        onWordMouseOver: getCallback('onWordMouseOver'),
+    };
+
+    function getCallback(callback) {
+        return function(word, event) {
+            const isActive = callback !== 'onWordMouseOut';
+            const element = event.target;
+            const text = select(element);
+            text
+                .on('click', () => {
+                    if (isActive) {
+                        window.open(`https://duckduckgo.com/?q=${word.text}`, '_blank');
+                    }
+                })
+                .transition()
+                // .attr('background', 'white')
+                // .attr('font-size', isActive ? '300%' : '100%')
+                .attr('text-decoration', isActive ? 'underline' : 'none');
+        };
+    }
+
+    return (
+        <ReactWordcloud callbacks={callbacks} options={options} words={words} />
+    )
+};
