80230c06b9a7a774eda50a4adc2f9e7ff84508f2
rended <rended83@gmail.com>
2020-06-15 17:13:30 +0200

    images

--- component/storylet-viewer/src/style/StyledSlide.js
+++ component/storylet-viewer/src/style/StyledSlide.js
@@ -23,9 +23,8 @@ const SlideStyle = styled.div`
             };
   position: relative;
   background: url(${(props) => props.background});
-  background-color: ${(props) => props.backgroundColor};
+  background-color: ${(props) => props.backgroundColor || 'var(--storylet-white)'};
   background-size: cover;
-  background-color: var(--storylet-white);
   box-shadow: ${ (props) => props.isParent ?  '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,1)' : '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)' };
   border-radius: 4px
 `;
